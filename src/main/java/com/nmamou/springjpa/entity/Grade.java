afba9242209b1b1af1087e6e790c62a11b4ddf05
Nacer Mamou <mamou.nacer@outlook.com>
2022-10-21 18:52:48 +0200

    Adding Constraints: prevent multiple grades for same studentsame course + no multiple courses with the same code

--- src/main/java/com/nmamou/springjpa/entity/Grade.java
+++ src/main/java/com/nmamou/springjpa/entity/Grade.java
@@ -1,4 +1,5 @@
 package com.nmamou.springjpa.entity;
+
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -11,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -23,26 +25,28 @@ import lombok.*;
 @AllArgsConstructor
 @RequiredArgsConstructor
 
-
-@Table(name="grade")
+@Table(name = "grade", uniqueConstraints = {
+		@UniqueConstraint(columnNames = { "student_id", "course_id" })
+})
 public class Grade {
+	// Constraints added to prevent associating multiple grades for the same student
+	// id coupled with same course id
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
-    
-    @Column(name = "score", nullable = false)
-    private String score;
+	@Column(name = "score", nullable = false)
+	private String score;
 
-    // If optional is not set to false the grade will be registred even if the student_id is not specified
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "student_id" ,referencedColumnName = "id")
-    private Student student;
-    
-    
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "course_id" ,referencedColumnName = "id")
-    private Course course;
+	// If optional is not set to false the grade will be registred even if the
+	// student_id is not specified
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "student_id", referencedColumnName = "id")
+	private Student student;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "course_id", referencedColumnName = "id")
+	private Course course;
 
 }
